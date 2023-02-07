3360d3ae3c459bdfd8e7b82a5ed5340bf7149121
Tomáš Marek <87856666+BeepBoopBobo@users.noreply.github.com>
2023-02-07 22:47:32 +0100

    Add project files.

---
+++ genericRPG/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericRPG
+{
+    internal class Enemy
+    {
+        private int id;
+        private string name;
+        private int maxHealthPoints;
+        private int currHealthPoints;
+        private int damage;
+        private bool alive;
+        private object[] inventory= new object[4];
+
+
+        public int MaxHealthPoints { get { return maxHealthPoints; } }
+        public int CurrentHealthPoints { get { return currHealthPoints; } }
+        public int Damage { get { return damage;} }
+        public object Inventory { get { return inventory; } }
+
+        Enemy(int maxHealthPoints, int damage, object[] inventory)
+        {
+            this.damage = damage;
+            this.maxHealthPoints = maxHealthPoints;
+            this.currHealthPoints=  maxHealthPoints;
+            this.inventory = inventory;
+            this.alive = true;
+        }
+
+
+        public void changeHealthPoints(int healthPoints){
+            this.currHealthPoints += healthPoints;
+            if(healthPoints > 0)
+            {
+                Console.WriteLine("{0} healed for {1} points.", this.name, healthPoints);
+            }else if(healthPoints<0){
+                Console.WriteLine("{0} took {1} points of damage.", this.name, healthPoints);
+            }
+            else
+            {
+                Console.WriteLine("{0}'s health remained the same.", this.name);
+            }
+
+            if (this.currHealthPoints <= 0)
+            {
+                this.alive = false;
+                Console.WriteLine("{0} died.", this.name);
+            }
+        }
+
+        public object getItemFromInventory(int index)
+        {
+            Console.WriteLine("Looted {0}.", this.inventory[index]);
+            return inventory[index];
+        }
+    }
+}
---
+++ genericRPG/Item.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericRPG
+{
+    internal class Item
+    {
+        private int id;
+        private string name;
+        private string type;//consumable, weapon, ...
+        //TODO: requirements[]
+        private int value;
+        private string description;
+
+        public int Id { get; }
+        public string Name { get; }
+        public string Type { get; }
+        public int Value { get; }
+        public string Description { get { return description; } }
+
+
+
+        // example <'weapon', [damage, range]>
+        // example <'consumable', [heal, sideEffect]>
+        //maybe resit pres dedicnost
+        private Dictionary<string, string> props= new Dictionary<string, string>();
+
+        public void useItem(Item item)
+        {
+            /*
+            TODO: work with props
+            */
+            Console.Write("{0} used.", this.name);
+        }
+    }
+}
